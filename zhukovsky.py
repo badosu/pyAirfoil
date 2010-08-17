b6a69ce8b63e87438764b9b9224bbaf1ac71396a
Amadeus Folego <amadeusfolego@gmail.com>
2010-08-17 03:55:04 -0300

    Working, needs correction on integrand

--- zhukovsky.py
+++ zhukovsky.py
@@ -1,66 +1,89 @@
 #!/usr/bin/env python
 
-#IMPORTACAO------------------------------------
+#IMPORTS---------------------------------------
 from pylab import *
 from scipy.integrate import quad
-from scipy.io.numpyio import fwrite, fread
 #----------------------------------------------
 
-#CONFIGURACOES N: numero de paineis -----------
-N = 50
+#CONFIGS N: number of panels ------------------
+N=50
 twopi = 2*pi
 #----------------------------------------------
 
-#CONSTANTES a, alpha, beta, uinf
-a=0.75
-alphadg = 7
-alpha=radians(alphadg)
-beta=-0.07
-uinf = 1
+#CONSTANT a, alpha, beta, uinf
+a = 0.75
+alphadg = 5
+alpha = radians(alphadg)
+beta=-0.05
+Uinf = 1
 #----------------------------------------------
 
-#FUNCOES --------------------------------------
+#FUNCTIONS-------------------------------------
 
 def Zeta(theta):
-    return (cos(theta)+beta)+(sin(theta)+alpha)*1j;
+	return (cos(theta)+beta)+(sin(theta)+alpha)*1j;
 
 def Zhukovsky(zeta):
-    return zeta+a*pow(zeta,-1)
+	return zeta+a*pow(zeta,-1)
 
 #----------------------------------------------
 
 #DEFINE theta, zeta, z-------------------------
 theta = arange(0, twopi, twopi/N)
-print 'len(theta)={0}'.format(len(theta))
-print 'N = {0}, 2pi/N={1}'.format(N, twopi/N)
 zeta = Zeta(theta)
 z = Zhukovsky(zeta)
+x = z.real; y = z.imag
 #----------------------------------------------
 
-#EXIBICAO INICIAL------------------------------
-figure(1)
+#INITIAL VISUALIZATION-------------------------
+
+figure()
 plot(zeta.real, zeta.imag,'g',label=r'$\zeta(\theta) = \xi + i\eta$')
-plot(z.real, z.imag,'r-o',label=r'$z(\theta) = x + iy$')
+plot(x, y,'r-o',label=r'$z(\theta) = x + iy$')
 plot(beta, alpha, 'g+')                    
 grid()
 legend()
 axis('scaled')
 title('Aerofolio fino')
 xlabel(r'$a = {0},\, \alpha = {1} graus, \beta = {2}$'.format(a, alphadg, beta)) 
-plot(0,0,'k+',ms=10)
+plot(0, 0, 'k+', ms=10)
 #-----------------------------------------------
 
-#CALCULA MATRIX DE COEF DE INFLUENCIA-----------
-A = mat(zeros((N, N)))
+#GET COEFFICIENTS INFLUENCE MATRIX AND APPLY KUTTA CONDITION
+A = mat(zeros((N+1,N+1)))
+A[N,0]  =  1
+A[N,N-1]= -1
+
 for i in range(N):
+	A[i,N]=1
 	for j in range(N):
-		A[i,j] = (quad(lambda t: log(abs((z[i]+(z[(i+1)%N])/2-Zhukovsky(Zeta(t))))), theta[j], theta[(j+1)%N])[0])/twopi
+		if i==j :
+			A[i,i]=0.5
+		else:
+			integrand = lambda t: log(abs((z[i]+z[(i+1)%N])/2-Zhukovsky(Zeta(t))))
+			A[i,j] = quad(integrand, theta[j], theta[(j+1)%N])[0]/twopi
+
+
+b = Uinf*(concatenate((y,[0])))
 #-----------------------------------------------
 
-#RESOLVE SISTEMA LINEAR-------------------------
-gamma = zeros(N); y = z.imag
+#SOLVE LINEAR SYSTEM TO OBTAIN gamma AND Cp-----
+sol = solve(A, b)
+
+gamma = sol[0:N]
+C = sol[N]
+
+print 'C={0}'.format(C)
+
+Cp = 1-pow(gamma,2)/(Uinf**2)
 #-----------------------------------------------
 
-#EXIBE TUDO-------------------------------------
+#PLOT EVERYTHING, SHOW AND SAVE-----------------
+
+figure()
+xn = x - min(x)
+plot(xn/max(xn), -Cp, 'b-o', label = r'$-C_p(x)$')
+legend()
+
 show()
 #-----------------------------------------------
